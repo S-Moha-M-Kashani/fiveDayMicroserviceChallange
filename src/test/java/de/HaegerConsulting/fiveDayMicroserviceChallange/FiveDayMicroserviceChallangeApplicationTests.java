5b57312514c2c2a2cc9349f1fee135896b98057b
Seyed Mohammad Mostafavi Kashani (Moha) <83539887+S-Moha-M-Kashani@users.noreply.github.com>
2021-09-08 15:35:57 +0200

    initial commit!

---
+++ src/test/java/de/HaegerConsulting/fiveDayMicroserviceChallange/FiveDayMicroserviceChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.HaegerConsulting.fiveDayMicroserviceChallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiveDayMicroserviceChallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
